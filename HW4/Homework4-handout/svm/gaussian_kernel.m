8f95a6c7cab6ada7aae837481828df3b1c2a3aad
Aravind Selvan <aravind646@gmail.com>
2014-10-17 15:47:37 -0400

    added kernels

--- HW4/Homework4-handout/svm/gaussian_kernel.m
+++ HW4/Homework4-handout/svm/gaussian_kernel.m
@@ -1,5 +1,8 @@
 function kernel = gaussian_kernel(x, z)
 % returns a gaussian kernel matrix with gamma == 1/2 of vector x and vector z
 % x and z must be in the same dimension
-    % YOUR CODE HERE
+    % CODE BEGIN
+    gamma = 1/2;
+    kernel = exp(-gamma*(sumsqr(x-z)));
+    % CODE END
     
--- HW4/Homework4-handout/svm/polynomial_kernel.m
+++ HW4/Homework4-handout/svm/polynomial_kernel.m
@@ -1,4 +1,8 @@
 function kernel = polynomial_kernel( x, z )
 % returns a polynomial kernel matrix with d = 2
-    % YOUR CODE HERE
-   
+   % CODE BEGIN
+   d = 2;
+   kernel = (dot(x,z) + 1)^d;
+   % CODE END
+
+    
